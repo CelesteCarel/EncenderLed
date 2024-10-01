cf566bad8a7f8484d2af59a8c68c1f84322fd8ae
CelesteCarel <Celeste.corvcab787@gmail.com>
2024-10-01 14:45:14 -0600

    Agregar archivos de proyecto.

---
+++ Form1.Designer.cs
@@ -0,0 +1,88 @@
+﻿namespace EncenderLed
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Encender = new System.Windows.Forms.Button();
+            this.Apagar = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // Encender
+            // 
+            this.Encender.Location = new System.Drawing.Point(102, 91);
+            this.Encender.Name = "Encender";
+            this.Encender.Size = new System.Drawing.Size(89, 39);
+            this.Encender.TabIndex = 0;
+            this.Encender.Text = "Encender";
+            this.Encender.UseVisualStyleBackColor = true;
+            this.Encender.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Apagar
+            // 
+            this.Apagar.Location = new System.Drawing.Point(102, 146);
+            this.Apagar.Name = "Apagar";
+            this.Apagar.Size = new System.Drawing.Size(89, 31);
+            this.Apagar.TabIndex = 1;
+            this.Apagar.Text = "Apagar";
+            this.Apagar.UseVisualStyleBackColor = true;
+            this.Apagar.Click += new System.EventHandler(this.Apagar_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(114, 48);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(77, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Encender LED";
+            // 
+            // Form1
+            // 
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.Apagar);
+            this.Controls.Add(this.Encender);
+            this.Name = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnEncender;
+        private System.Windows.Forms.Button btnApagar;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button Encender;
+        private System.Windows.Forms.Button Apagar;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO.Ports;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace EncenderLed
+{
+    public partial class Form1 : Form
+    {
+        public SerialPort ArduinoPort { get; }
+        public Form1()
+        {
+            InitializeComponent();
+
+            ArduinoPort = new System.IO.Ports.SerialPort();
+            ArduinoPort.PortName = "COM4";
+            ArduinoPort.BaudRate = 9600;
+            ArduinoPort.Open();
+
+            this.FormClosing += CerrandoForm1;
+            this.Apagar.Click += Apagar_Click;
+            this.Encender.Click += button2_Click;
+
+        }
+        private void CerrandoForm1(object sender, EventArgs e)
+        {
+            if (ArduinoPort.IsOpen) ArduinoPort.Close();
+
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            ArduinoPort.Write("a");
+        }
+
+        private void Apagar_Click(object sender, EventArgs e)
+        {
+            ArduinoPort.Write("b");
+        }
+        private void Cerrandoform1(object sender, EventArgs e)
+        {
+            if (ArduinoPort.IsOpen) ArduinoPort.Close();
+        }
+
+    }
+}
